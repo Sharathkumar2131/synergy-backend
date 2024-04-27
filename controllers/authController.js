a86959fdecd4c4ef288896c2b1d65c801ce90196
Sharathkumar thanugula <sharathkumarthanugula@gmail.com>
2024-04-27 13:55:58 +0530

    changes auth

--- controllers/authController.js
+++ controllers/authController.js
@@ -11,33 +11,28 @@ const config = {
   },
 };
 
-
 async function loginUser(req, res) {
-  const { id, name } = req.body;
+  const { id, role } = req.body;
 
   try {
     const pool = await sql.connect(config);
-    const request = pool.request();
-    request.input('id', sql.NVarChar(50), id); // Use string format for id
-    request.input('name', sql.NVarChar(50), name); // Change 'role' to 'name'
-
-    const result = await request.query('SELECT * FROM Users WHERE id = @id AND name = @name'); // Change 'role' to 'name'
+    const result = await pool
+      .request()
+      .input('id', sql.NVarChar(50), id) // Use string format for id
+      .input('role', sql.NVarChar(50), role)
+      .query('SELECT * FROM cha WHERE id = @id AND role = @role');
 
     if (result.recordset.length > 0) {
       res.status(200).json({ message: 'Login successful' });
     } else {
       res.status(401).json({ message: 'Invalid credentials' });
     }
-
     await pool.close();
   } catch (err) {
     console.error('SQL error:', err.message);
     res.status(500).send('Internal server error');
   }
 }
-
-
-
 
 
 module.exports = {
