6e83da42ace701b9349e22f3e5fc27fda3e84c17
Sharathkumar thanugula <sharathkumarthanugula@gmail.com>
2024-04-27 13:51:17 +0530

    changes auth

--- controllers/authController.js
+++ controllers/authController.js
@@ -10,36 +10,35 @@ const config = {
       encrypt: false,
   },
 };
+
+
 async function loginUser(req, res) {
-  const { userId, user_roll } = req.body;
-
-  // Ensure userId is parsed as an integer
-  const parsedUserId = parseInt(userId);
-
-  // Check if userId is a valid integer
-  if (isNaN(parsedUserId)) {
-    return res.status(400).json({ message: 'Invalid userId. Must be a valid integer.' });
-  }
+  const { id, name } = req.body;
 
   try {
     const pool = await sql.connect(config);
-    const result = await pool
-      .request()
-      .input('userId', sql.Int, parsedUserId) // Use parsed integer value
-      .input('userRole', sql.NVarChar(50), user_roll)
-      .query('SELECT * FROM Users WHERE userId = @userId AND user_roll = @userRole');
+    const request = pool.request();
+    request.input('id', sql.NVarChar(50), id); // Use string format for id
+    request.input('name', sql.NVarChar(50), name); // Change 'role' to 'name'
+
+    const result = await request.query('SELECT * FROM Users WHERE id = @id AND name = @name'); // Change 'role' to 'name'
 
     if (result.recordset.length > 0) {
       res.status(200).json({ message: 'Login successful' });
     } else {
       res.status(401).json({ message: 'Invalid credentials' });
     }
+
     await pool.close();
   } catch (err) {
     console.error('SQL error:', err.message);
     res.status(500).send('Internal server error');
   }
 }
+
+
+
+
 
 module.exports = {
   loginUser
